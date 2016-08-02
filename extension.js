c9f9bc9b9e663a1ae8a40757061c4e93980db9de
ArceusMaster0493 <ArceusMaster0493@users.noreply.github.com>
2016-08-02 14:17:53 +0800

    Update extension.js

--- extension.js
+++ extension.js
@@ -125,7 +125,7 @@
       voteSkipLimit: 12,
       historySkip: true,
       timeGuard: true,
-      maximumSongLength: 7,
+      maximumSongLength: 6,
       autodisable: true,
       commandCooldown: 5,
       usercommandsEnabled: true,
