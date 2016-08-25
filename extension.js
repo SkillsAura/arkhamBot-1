1a8628fd6dc0f2bdf35f2e3dc8d94e2d760de142
ArceusMaster0493 <ArceusMaster0493@users.noreply.github.com>
2016-08-25 22:55:59 +0800

    Update extension.js

--- extension.js
+++ extension.js
@@ -24,11 +24,10 @@
           window.bot.chatUtilities.spam.push(spamWords[i]);
         }
 
-        // Example code for a bot command:
         bot.commands.mwarCommand = {
-          command: 'mwar',  // The command to be called. With the standard command literal this would be: !bacon
-          rank: 'user', // Minimum user permission to use the command
-          type: 'exact', // Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+          command: 'mwar',
+          rank: 'user',
+          type: 'exact',
           functionality: function (chat, cmd) {
             if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
             if (!bot.commands.executable(this.rank, chat)) return void (0);
