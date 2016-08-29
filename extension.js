22c9c1975a0ec28a7a53c3387c03e01f5df1bb0b
ArceusMaster0493 <ArceusMaster0493@users.noreply.github.com>
2016-08-29 22:10:43 +0800

    Update extension.js

--- extension.js
+++ extension.js
@@ -101,7 +101,7 @@
       botName: "arkhamBot",
       language: "english",
       chatLink: "https://rawgit.com/ArceusMaster0493/arkhamBot/master/lang/en.json",
-      scriptLink: "https://rawgit.com/basicBot/source/master/basicBot.js",
+      scriptLink: "https://rawgit.com/ArceusMaster0493/arkhamBot/master/script/basicBot.js",
       roomLock: false, // Requires an extension to re-load the script
       startupCap: 1, // 1-200
       startupVolume: 0, // 0-100
